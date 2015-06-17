25d5c5b3bafa38d582bdb8d5ce00518816af10ce
Ronald Lima <ronald.lima@repplica.com.br>
2015-06-17 00:29:34 -0300

    Initial

---
+++ TimeSync/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TimeSync
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+		}
+	}
+}
---
+++ serverTimeSync/MyClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ServerTimeSync
+{
+	public class MyClass
+	{
+		public MyClass ()
+		{
+		}
+	}
+}
+
